f1834b6e4d9fc4007151a03d837ac2a83ec37e58
Anna Kazmirchuk <anyuta.kazmirchuk32@gmail.com>
2022-04-21 16:15:58 +0300

    New commit

--- phones/views.py
+++ phones/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from phones.models import Phone
 
@@ -10,23 +10,31 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     phones = Phone.objects.all()
+    SORT_MAP = {'name': 'name',
+                'min_price': 'price',
+                'max_price': '-price'
+                }
+
     sort = request.GET.get('sort')
+    if sort:
+        phones = phones.order_by(SORT_MAP[sort])
 
-    if sort == 'name':
-        result = phones.order_by('name')
-    elif sort == 'min_price':
-        result = phones.order_by('price')
-    elif sort == 'max_price':
-        result = phones.order_by('-price')
-    else:
-        result = phones
+    # if sort == 'name':
+    #     result = phones.order_by('name')
+    # elif sort == 'min_price':
+    #     result = phones.order_by('price')
+    # elif sort == 'max_price':
+    #     result = phones.order_by('-price')
+    # else:
+    #     result = phones
 
-    context = {'phones': result}
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phone = Phone.objects.get(slug=slug)
+    # phone = Phone.objects.get(slug=slug)
+    phone = get_object_or_404(Phone, slug=slug)
     context = {'phone': phone}
     return render(request, template, context)
